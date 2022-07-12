f65246fd384dcf91451ae7469e0cadce91d7d83f
ocariocawebdesign <ocariocawebdesign@gmail.com>
2022-07-12 12:15:52 -0300

    first commit

---
+++ src/config/login-social-config.js
@@ -0,0 +1,7 @@
+export const getGoogleID = () => {
+    return '800889458901-tgd8r2qdif1oehdd33prgmre5690olea.apps.googleusercontent.com';
+}
+ 
+export const getFacebookID = () => {
+    return '800429407623865';
+}
